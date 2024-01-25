911fbd728a719dd717b13b486e2c6415c469a2a1
DimRev <dimarev444@gmail.com>
2024-01-25 16:29:01 +0200

    Add count to page

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MarketerForm from '../cmps/MarketerForm'
+import { formService } from '../services/api/form.service'
 
 export default function FormPage() {
   const [isSubmitted, setIsSubmitted] = useState(false)
+  const [formsCount, setFormsCount] = useState(0)
+  useEffect(() => {
+    getFormsCount()
+  }, [isSubmitted])
+
+  async function getFormsCount() {
+    const forms = await formService.getForms()
+    setFormsCount(forms.length)
+  }
   return (
     <section className="form-page">
+      <div>{formsCount} have already sent a form!</div>
       {isSubmitted ? (
         <div className="submit-msg">Thank you for submitting the form</div>
       ) : (
