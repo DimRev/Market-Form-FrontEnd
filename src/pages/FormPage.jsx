ca54688bd8724389e29df9353feca3080db0d683
DimRev <dimarev444@gmail.com>
2024-01-25 13:42:06 +0200

    Add FormPage pageCmp

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import MarketerForm from '../cmps/MarketerForm'
 
 export default function FormPage() {
-  return <section className="form-page"></section>
+  return (
+    <section className="form-page">
+      <MarketerForm />
+    </section>
+  )
 }
